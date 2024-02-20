944d7eda5ed4ee56553d26762e1a7b8797cdbd27
skurscheid <sebastian@tonttu.ai>
2024-02-20 17:23:15 +0100

    initial commit

---
+++ neo4j_and_llms/tutorial.py
@@ -0,0 +1,14 @@
+from langchain_openai import OpenAI
+from langchain.prompts import Prompt
+
+# load a prompt template
+with open("./prompt_template_cockney_fruit_seller.txt", "r") as file:
+    prompt_template = file.read()
+
+template = Prompt(template = prompt_template, input_variables=["fruit"])
+
+llm = OpenAI(openai_api_key="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")
+
+response = llm.invoke(template.format(fruit="peaches"), model="gpt-3.5-turbo-instruct", max_tokens=100, temperature=0.9, top_p=1, frequency_penalty=0, presence_penalty=0)
+
+print(response)
