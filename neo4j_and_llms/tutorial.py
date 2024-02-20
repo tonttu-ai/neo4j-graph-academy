28a37a108091cd3cdcaf54a76152ee2b8f5bf31e
skurscheid <sebastian@tonttu.ai>
2024-02-20 18:31:58 +0100

    concludes the first langchain tutorial

--- neo4j_and_llms/tutorial.py
+++ neo4j_and_llms/tutorial.py
@@ -1,8 +1,10 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import OpenAI
-from langchain.prompts import Prompt
 from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
+from langchain.schema import StrOutputParser
+from langchain.output_parsers.json import SimpleJsonOutputParser
 
 # load the .env file
 load_dotenv()
@@ -15,8 +17,22 @@ with open("./prompt_template_cockney_fruit_seller.txt", "r") as file:
 template = PromptTemplate(template=prompt_text, input_variables=["fruit"])
 template.save("cockney_fruit_seller.json")
 
-llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+# create a new LLM
+llm = OpenAI(api_key=os.getenv("OPENAI_API_KEY"),
+             model="gpt-3.5-turbo-instruct", 
+             max_tokens=100, 
+             temperature=0.9, # cranks up the creativity, more suitable to a cockney fruit seller
+             top_p=1,
+             frequency_penalty=0, 
+             presence_penalty=0)
 
-response = llm.invoke(template.format(fruit="peaches"), model="gpt-3.5-turbo-instruct", max_tokens=100, temperature=0.9, top_p=1, frequency_penalty=0, presence_penalty=0)
+# create a new LLMChain
+llm_chain = LLMChain(
+    llm=llm,
+    prompt=template,
+    output_parser=SimpleJsonOutputParser()
+)
+
+response = llm_chain.invoke({"fruit": "apples"})
 
 print(response)
