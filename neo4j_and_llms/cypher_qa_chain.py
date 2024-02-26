5b8dd04f9d8c3d9d54a625abc9277700a0dd6647
Sebastian Kurscheid <skurscheid@gmail.com>
2024-02-26 17:25:47 +0100

    Update Cypher query to retrieve the genre of a movie

--- neo4j_and_llms/cypher_qa_chain.py
+++ neo4j_and_llms/cypher_qa_chain.py
@@ -25,6 +25,21 @@ CYPHER_GENERATION_TEMPLATE = """
 You are an expert Neo4j Developer translating user questions into Cypher to answer questions about movies and provide recommendations.
 Convert the user's question based on the schema.
 
+Instructions:
+Use only the provided relationship types and properties in the schema.
+Do not use any other relationship types or properties that are not provided.
+For movie titles that begin with "The", move "the" to the end, For example "The 39 Steps" becomes "39 Steps, The" or "The Matrix" becomes "Matrix, The".
+
+If no data is returned, do not attempt to answer the question.
+Only respond to questions that require you to construct a Cypher statement.
+Do not include any explanations or apologies in your responses.
+
+Examples:
+
+Find movies and genres:
+MATCH (m:Movie)-[:IN_GENRE]->(g)
+RETURN m.title, g.name
+
 Schema: {schema}
 Question: {question}
 """
@@ -41,4 +56,4 @@ cypher_chain = GraphCypherQAChain.from_llm(
     verbose=True
 )
 
-cypher_chain.invoke({"query": "What role did Tom Hanks play in Toy Story?"})
+cypher_chain.invoke({"query": "What genre of film is Toy Story?"})
